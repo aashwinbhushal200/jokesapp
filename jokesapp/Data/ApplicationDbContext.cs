ea00443e4f2b09060c6368148218a2f705c9ae05
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2023-06-13 00:22:11 +0530

    Add project files.

---
+++ jokesapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using jokesapp.Models;
+
+namespace jokesapp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<jokesapp.Models.Joke> Joke { get; set; }
+    }
+}
